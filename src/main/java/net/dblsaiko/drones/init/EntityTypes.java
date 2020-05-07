51c66859c77249d55497540f60637adb207b9035
2xsaiko <git@dblsaiko.net>
2020-05-07 09:07:29 +0200

    Update to 20w19a

--- src/main/java/net/dblsaiko/drones/init/EntityTypes.java
+++ src/main/java/net/dblsaiko/drones/init/EntityTypes.java
@@ -1,11 +1,11 @@
 package net.dblsaiko.drones.init;
 
-import net.minecraft.entity.EntityCategory;
 import net.minecraft.entity.EntityDimensions;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
-import net.fabricmc.fabric.api.entity.FabricEntityTypeBuilder;
+import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
 
 import net.dblsaiko.drones.Main;
 import net.dblsaiko.drones.entity.DroneEntity;
@@ -16,9 +16,9 @@ public class EntityTypes {
 
     public static void initialize() {
         DRONE = Registry.register(Registry.ENTITY_TYPE, new Identifier(Main.MODID, "drone"),
-            FabricEntityTypeBuilder.create(EntityCategory.MISC, DroneEntity::new)
-                .size(EntityDimensions.fixed(0.75f, 0.3f))
-                .trackable(64, 4).build());
+                FabricEntityTypeBuilder.create(SpawnGroup.MISC, DroneEntity::new)
+                        .dimensions(EntityDimensions.fixed(0.75f, 0.3f))
+                        .trackable(64, 4).build());
     }
 
 }
